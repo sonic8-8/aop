597406379b816a65b4d22490eb842f8cdd23c786
이승환 <seaung13@gmail.com>
2025-03-11 19:08:25 +0900

    feat: 스프링 AOP - 예제 프로젝트에 로그 출력 AOP 적용

--- src/test/java/hello/aop/exam/ExamTest.java
+++ src/test/java/hello/aop/exam/ExamTest.java
@@ -1,10 +1,13 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.TraceAspect;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TraceAspect.class)
 public class ExamTest {
 
     @Autowired
